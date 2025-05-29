8a44bb2a57199409d174a722d3f31c0e285c642e
growagardenv3 <davisallison504@gmail.com>
2025-05-29 19:25:51 +0800

    Delete Blol.lua

--- Blol.lua
+++
@@ -1,9 +0,0 @@
-local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/growagardenv3/gui/refs/heads/main/gagupdated.lua.txt"))()
-end)
-
-if success then
-    print("Script loaded successfully!")
-else
-    warn("Error loading script:", err)
-end
